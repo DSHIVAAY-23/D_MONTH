2df53f5a27a0ba3182cc7f8da020708d0f6d997d
DSHIVAAY-23 <raidivyank08@gmail.com>
2025-05-20 17:31:13 +0530

    D month

---
+++ Binary Typewriter.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vec vector<long long> 
+#define pi pair<int,int>
+#define D for(int i =0;i<n;i++)
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << "\n";
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void solve() {
+   string s;
+    int n;
+    cin >> n;
+    cin >> s;
+    s="0"+s;
+    int ans = 0, cur = s[0];
+    for (int i = 1; i <= n; i++) {
+        int dig = s[i];
+        if (cur != dig) 
+           ans++;
+        cur = dig;
+    }
+   if(ans>=3)
+     cout<< ans-2 + n<<"\n";
+   else if(ans==2)
+     cout<< ans-1 + n<<"\n";
+   else 
+     cout<<ans+n<<"\n";      
+}
+
+signed main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
